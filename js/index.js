ec6c71c818e43a52e61d0b1ce00e6bcdf00462ee
zaheerabbas216 <abbaszaheer216@gmail.com>
2022-08-20 01:15:56 +0530

    fixes and changes

--- js/index.js
+++ js/index.js
@@ -1,2 +1,47 @@
 console.log('js loaded');
 AOS.init();
+var $jq = jQuery.noConflict();
+jQuery(document).ready(function ($) {
+    $('.multiple-items').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true    
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+    // Your code here
+});
+// $jq(document).ready(function() { 
+//   $jq('a').click( function(event) {
+//     $jq(this).hide();
+//     event.preventDefault();
+//   });
+// });
